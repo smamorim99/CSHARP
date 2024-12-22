bf3ff1b9c114a2b175534bde04b5846d845da869
Sofia Mendes <sofiamendez.amorim99@outlook.com.br>
2024-12-22 14:08:18 -0300

    newcommit

---
+++ DesafioDeCodigo/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] pedidosIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+foreach (string pedido in pedidosIDs){
+    if (pedido.StartsWith("B")){
+        Console.WriteLine($"pedido suspeito {pedido}");
+    }
+
+}
