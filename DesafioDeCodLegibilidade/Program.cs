04de53069c61c22ad714607df3835a9c7db6d2a4
Sofia Mendes <sofiamendez.amorim99@outlook.com.br>
2024-12-22 15:11:22 -0300

    desafio concluido

---
+++ DesafioDeCodLegibilidade/Program.cs
@@ -0,0 +1,18 @@
+﻿/* O codigo inverte a menssagem, conta o numero de vezes que o caracter "O" aparece,
+então imprime o resultado no terminal.*/
+
+
+string message = "The quick brown fox jumps over the lazy dog.";
+char[] charMessage = message.ToCharArray();
+Array.Reverse(charMessage);
+
+int numeroDeRepetição = 0;
+foreach (char letra in charMessage){ 
+    if (letra == 'o'){ 
+    numeroDeRepetição++; 
+    } 
+}
+string newMessage = new String(charMessage);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {numeroDeRepetição} times.");
