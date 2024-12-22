ddf121fd41836f125f294d1124d4d33ca52be5ca
Sofia Mendes <sofiamendez.amorim99@outlook.com.br>
2024-12-22 13:35:18 -0300

    matriz usando int, foreach

---
+++ MatrizDeInt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+int[] inventario = {200, 450, 700, 175, 250};
+int soma = 0;
+int bin = 0;
+foreach (int itens in inventario){
+soma += itens;
+bin++;
+Console.WriteLine($"Bin {bin} = {itens} items (mostrando o total: {soma})");
+
+}
+Console.WriteLine($"existem {soma} itens no inventário");
