3c06b0b1227bc05c6f535daeaee20f4156935470
Sofia Mendes <sofiamendez.amorim99@outlook.com.br>
2024-12-30 11:59:14 -0300

    estudo

---
+++ EstudoOperadorCondicional/Program.cs
@@ -0,0 +1,4 @@
+﻿int valorDaVenda = 800;
+//int desconto = valorDaVenda > 1000 ? 100 : 50;
+//Console.WriteLine("Desconto:" + (valorDaVenda > 1000 ? 100 : 50));
+Console.WriteLine($"Desconto:{(valorDaVenda > 1000 ? 100 : 50)}");
