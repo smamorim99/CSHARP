73caa746971a34d70999d4a146d4a8cb07e15763
Sofia Mendes <sofiamendez.amorim99@outlook.com.br>
2024-12-31 17:56:46 -0300

    exercicio de cod

---
+++ ExercicioBlocosDeCod/Program.cs
@@ -0,0 +1,9 @@
+﻿string name = "Steve";
+
+if (name == "bob") 
+    Console.WriteLine("found bob");
+else if 
+    (name == "Steve") Console.WriteLine("found Steve");
+else 
+    Console.WriteLine("found Chuck");
+
