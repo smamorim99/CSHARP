dd6c43cd4f972663f1043f2acfa41f9d77ea340f
Sofia Mendes <sofiamendez.amorim99@outlook.com.br>
2024-12-31 21:05:20 -0300

    desafio switch

---
+++ ExercicioSwithCase/Program.cs
@@ -0,0 +1,21 @@
+﻿int lvlFuncionario = 100;
+string nomeFuncionario = "Jhon Smith";
+
+string titulo = "";
+
+switch (lvlFuncionario){
+    case 100: 
+    case 200: 
+    titulo = "Assistente Senior";
+    break;
+    case 300:
+    titulo = "Gerente";
+    break;
+    case 400:
+    titulo = "Gerente Senior";
+    break;
+    default:
+    titulo = "Associado";
+    break;
+}
+Console.WriteLine($"{nomeFuncionario}, {titulo}");
