24909304762f288100ee8fc2e6e494d03b376bb2
Sofia Mendes <sofiamendez.amorim99@outlook.com.br>
2025-01-03 11:49:23 -0300

    exercicio

---
+++ ExercicioInstrucaoWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿Random numeros = new();
+int current = numeros.Next(1, 11);
+
+/*do{
+    current = numeros.Next(1, 11);
+    Console.WriteLine(current);
+} while (current != 7);
+
+/*while (current >= 3){
+    Console.WriteLine(current);
+    current = numeros.Next(1, 11);
+}
+Console.WriteLine($"ultino numero: {current}");*/
+
+do{ 
+    current = numeros.Next(1,11);
+    
+    if (current >= 8)
+    continue;
+
+    Console.WriteLine(current);
+} while (current != 7);
