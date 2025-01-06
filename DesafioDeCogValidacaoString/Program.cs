7d560b5f3999d197f7b5bf4062c804b33c8c0b94
Sofia Mendes <sofiamendez.amorim99@outlook.com.br>
2025-01-06 15:04:41 -0300

    Desafio 2: concluido

---
+++ DesafioDeCogValidacaoString/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Entre com a sua função");
+bool funcao = false;
+
+do{
+    string entrada = Console.ReadLine().Trim().ToLower();
+        
+            if (entrada == "administrador" || entrada == "gerente" || entrada == "usuario")
+                funcao = true; 
+            else
+                Console.WriteLine($" {entrada} não é uma funçaõ valida, entre com uma função valida!");   
+
+}while (!funcao);
+    Console.WriteLine("Acesso aceito! Bem vindo!");
